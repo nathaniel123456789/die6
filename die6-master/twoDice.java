f3f227d818529492088248861edd79b7598c4b2b
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-11-09 11:39:21 -0800

    works sss

--- die6-master/twoDice.java
+++ die6-master/twoDice.java
@@ -5,7 +5,6 @@ public class twoDice
   public twoDice() {
     die1 = new Die6();
     die2 = new Die6();
-    this.roll();
   }
   public int getValue() {
     return die1.getValue() + die2.getValue();
